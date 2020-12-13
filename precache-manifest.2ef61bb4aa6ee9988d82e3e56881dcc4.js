ce1abd4f13ad83a67a0b9f237eee5d2741dd89c4
dzoladz <derek@derekzoladz.com>
2020-12-13 09:39:53 -0500

    Updates

---
+++ precache-manifest.2ef61bb4aa6ee9988d82e3e56881dcc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fef605d25b79bd451aad7f1717597a3",
+    "url": "/card-reader-app/index.html"
+  },
+  {
+    "revision": "3c2749b78d2bd1a9b05a",
+    "url": "/card-reader-app/static/css/main.c7fa886e.chunk.css"
+  },
+  {
+    "revision": "aa90f644a04112e66cc0",
+    "url": "/card-reader-app/static/js/2.5f26eff4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/card-reader-app/static/js/2.5f26eff4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c2749b78d2bd1a9b05a",
+    "url": "/card-reader-app/static/js/main.f48e662f.chunk.js"
+  },
+  {
+    "revision": "464dbe42d6d4bc2bb45c",
+    "url": "/card-reader-app/static/js/runtime-main.18dd0bc0.js"
+  }
+]);
