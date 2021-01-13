58ca7288a25904aea9f93dc5bfac1552a7c941f1
dzoladz <derek@derekzoladz.com>
2021-01-12 20:44:02 -0500

    Updates

--- precache-manifest.bd574fbe2a0c150fb94cd8ebfb440d59.js
+++ precache-manifest.bd574fbe2a0c150fb94cd8ebfb440d59.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "097bed3c641620ec1e43d54a65e55cca",
+    "revision": "3266a90a9f7928fb7e815539c6023b3d",
     "url": "/card-reader-app/index.html"
   },
   {
-    "revision": "fcbdfd44484fbf609de5",
+    "revision": "6fdc0bc9b56b18481a3a",
     "url": "/card-reader-app/static/css/main.e7b2a14e.chunk.css"
   },
   {
-    "revision": "3dad9f93547206908b2f",
-    "url": "/card-reader-app/static/js/2.cf9d6b25.chunk.js"
+    "revision": "6f037be09d921ffde765",
+    "url": "/card-reader-app/static/js/2.ca801c4c.chunk.js"
   },
   {
     "revision": "bfc08a62983de46a4daec9f8c3c657d8",
-    "url": "/card-reader-app/static/js/2.cf9d6b25.chunk.js.LICENSE.txt"
+    "url": "/card-reader-app/static/js/2.ca801c4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fcbdfd44484fbf609de5",
-    "url": "/card-reader-app/static/js/main.776be577.chunk.js"
+    "revision": "6fdc0bc9b56b18481a3a",
+    "url": "/card-reader-app/static/js/main.7f5d835f.chunk.js"
   },
   {
     "revision": "fa6cfc43ea955af75664",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/card-reader-app/precache-manifest.7ad6b894a59097fe4612522544e97417.js"
+  "/card-reader-app/precache-manifest.bd574fbe2a0c150fb94cd8ebfb440d59.js"
 );
 
 self.addEventListener('message', (event) => {
